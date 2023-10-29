30b1ab878461f9290d012484b8055f07c6e2a56f
danielfgmb <49533662+danielfgmb@users.noreply.github.com>
2023-10-29 16:08:10 -0500

    late change

--- app_django/wsgi.py
+++ app_django/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from ods_translator.load_joblib import df_preprocess, tokenize_df
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_django.settings')
+
+
 
 application = get_wsgi_application()
